61ee26d3940a95f15660bd7a7f357999c4d75219
FIRST_NAME LAST_NAME <annamakhortova@example.com>
2023-07-20 14:11:28 +0300

    /

--- utilis.js
+++ utilis.js
@@ -3,20 +3,21 @@ import { allComments, postComments, userData} from "./api.js";
 import { renderComments, renderForm} from "./renderComments.js";
 import { commentClickListener } from "./listeners.js";
 
-function formatDate(date) {
 
+function formatDate(date) {
+  
     let dd = date.getDate();
     if (dd < 10) dd = '0' + dd;
-
+    
     let mm = date.getMonth() + 1;
     if (mm < 10) mm = '0' + mm;
-
+    
     let yy = date.getFullYear() % 100;
     if (yy < 10) yy = '0' + yy;
-
+    
     let hh = date.getHours() % 100
     if (hh < 10) hh = '0' + hh;
-
+  
     let mi = date.getMinutes() % 100
     if (mi < 10) mi = '0' + mi;
     return dd + '.' + mm + '.' + yy + ' ' + hh + ':' + mi;
@@ -51,7 +52,7 @@ function delay(interval = 300) {
 
 function addNewComment() {
   let newComment = document.querySelector('.add-form-text')
-  let date = new Date();
+    let date = new Date();
     let loadedComment = true
     renderForm(loadedComment)
     postComments(newComment)
